1f9ad9390241c8adb82470f0b47713a2792cb44c
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2024-02-01 13:39:57 +0300

    feat: add login and signup pages
    Add login and signup pages to the app for user authentication

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+export default function Login() {
+  return (
+    <main className="bg-green-200 h-svh w-full grid place-items-center">
+    <form className=" bg-teal-400 text-black  px-6 py-10 rounded grid justify-center " action="/auth/login" method="post">
+      <label className=" text-lg font-semibold" htmlFor="email"> Login Email</label>
+      <input className="block  w-full my-2 py-3 px-4 rounded" name="email" />
+      <label className=" text-lg font-semibold" htmlFor="password">Password</label>
+      <input className="block  w-full my-2 py-3 px-4 rounded" type="password" name="password" />
+      <button className="bg-orange-400 py-3 px-4 rounded-md border-none text-center mt-14 hover:text-orange-400 hover:bg-green-200 transition-2000">Sign In</button>
+      {/* <button formAction="/auth/sign-up">Sign Up</button> */}
+    </form>
+    </main>
+  )
+}
