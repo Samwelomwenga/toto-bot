09274b4f8456809635c2de70ba159ebf47615a71
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2024-02-06 00:46:29 +0300

    feat: add animation to menu
    Add a fade in animation to the menu when it is opened and a fade out
    animation when it is closed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,16 +6,28 @@ function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
   return (
-    <header className="text-black bg-teal-600 h-20 w-full px-3 pt-5 shadow-lg">
+    <header className="text-black bg-teal-600 h-20 w-full px-3 pt-5 shadow-lg sticky">
       <div className="flex justify-between mb-8">
         <p className="text-2xl font-semibold text-orange-400">TOTO BOT</p>
-        {!isOpen ? <SlMenu className=" w-7 h-7 text-orange-400" onClick={toggle} /> : <VscChromeClose className=" w-7 h-7 text-orange-400"onClick={toggle} />}
+        {!isOpen ? (
+          <SlMenu
+            className={` w-7 h-7 text-orange-400 transition-transform duration-700 ease-in-out ${isOpen ? 'opacity-0 scale-0' : 'opacity-100 scale-100'}`}
+            onClick={toggle}
+          />
+        ) : (
+          <VscChromeClose
+            className={` w-7 h-7 text-orange-400  transition-transform duration-700 ease-in-out ${isOpen ? 'opacity-100 scale-100' : 'opacity-0 scale-0'}`}
+            onClick={toggle}
+          />
+        )}
       </div>
-    { isOpen? <ul className=" bg-orange-400  w-full h-48 rounded shadow-md">
-        <li className="text-2xl font-semibold text-orange-400">Home</li>
-        <li className="text-2xl font-semibold text-orange-400">About</li>
-        <li className="text-2xl font-semibold text-orange-400">Contact</li>
-      </ul>:null}
+      {isOpen ? (
+        <ul className=" bg-orange-400  w-full h-48 rounded shadow-md animate-fade-in-right">
+          <li className="text-2xl font-semibold text-orange-400">Home</li>
+          <li className="text-2xl font-semibold text-orange-400">About</li>
+          <li className="text-2xl font-semibold text-orange-400">Contact</li>
+        </ul>
+      ) : null}
     </header>
   );
 }
