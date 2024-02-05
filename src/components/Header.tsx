7d8e9f93cf5ddf0d5e9923c6b7e593544ea1b1d5
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2024-02-06 01:13:36 +0300

    fix: abstract menu into its own component
    abstract the menu into its own component to make it easier to manage and
    maintain the codebase

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,31 +2,28 @@
 import { useState } from "react";
 import { SlMenu } from "react-icons/sl";
 import { VscChromeClose } from "react-icons/vsc";
+import Menu from "./Menu";
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
   return (
     <header className="text-black bg-teal-600 h-20 w-full px-3 pt-5 shadow-lg sticky">
       <div className="flex justify-between mb-8">
-        <p className="text-2xl font-semibold text-orange-400">TOTO BOT</p>
         {!isOpen ? (
           <SlMenu
-            className={` w-7 h-7 text-orange-400 transition-transform duration-700 ease-in-out ${isOpen ? 'opacity-0 scale-0' : 'opacity-100 scale-100'}`}
-            onClick={toggle}
+          className={` w-7 h-7 text-orange-400 transition-transform duration-700 ease-in-out ${isOpen ? 'opacity-0 scale-0' : 'opacity-100 scale-100'}`}
+          onClick={toggle}
           />
-        ) : (
-          <VscChromeClose
+          ) : (
+            <VscChromeClose
             className={` w-7 h-7 text-orange-400  transition-transform duration-700 ease-in-out ${isOpen ? 'opacity-100 scale-100' : 'opacity-0 scale-0'}`}
             onClick={toggle}
-          />
-        )}
+            />
+            )}
+            <p className="text-2xl font-semibold text-orange-400">TOTO BOT</p>
       </div>
       {isOpen ? (
-        <ul className=" bg-orange-400  w-full h-48 rounded shadow-md animate-fade-in-right">
-          <li className="text-2xl font-semibold text-orange-400">Home</li>
-          <li className="text-2xl font-semibold text-orange-400">About</li>
-          <li className="text-2xl font-semibold text-orange-400">Contact</li>
-        </ul>
+       <Menu />
       ) : null}
     </header>
   );
---
+++ src/components/Menu.tsx
@@ -0,0 +1,12 @@
+
+function Menu() {
+  return (
+    <ul className=" bg-orange-400  w-full h-48 rounded shadow-md animate-fade-in-right">
+    <li className="text-2xl font-semibold text-orange-400">Home</li>
+    <li className="text-2xl font-semibold text-orange-400">About</li>
+    <li className="text-2xl font-semibold text-orange-400">Contact</li>
+  </ul>
+  )
+}
+
+export default Menu
