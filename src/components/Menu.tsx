a0760c60d9c8bc7e6a1327ecdf82b14e3686e708
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2024-02-07 02:04:49 +0300

    feat:add logout functionality
    Add logout functionality to the menu component to allow users to log out
     of the application

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,19 @@
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 
 function Menu({isOpen}:{isOpen: boolean}){
+  const supabase = createClientComponentClient();
+  const router = useRouter();
+  const handleSignOut = async () => {
+    await supabase.auth.signOut()
+    router.refresh()
+  }
+  
   return (
     <ul className={` bg-black   h-48 rounded shadow-md animate-fade-in-right  m-h-svh ${isOpen?"block":"hidden"} md:block w-5/6 min-h-screen z-20`}>
     <li className="text-2xl font-semibold text-orange-400">Home</li>
     <li className="text-2xl font-semibold text-orange-400">About</li>
-    <li className="text-2xl font-semibold text-orange-400"><a>Logout</a></li>
+    <li className="text-2xl font-semibold text-orange-400" onClick={handleSignOut}><a>Logout</a></li>
   </ul>
   )
 }
