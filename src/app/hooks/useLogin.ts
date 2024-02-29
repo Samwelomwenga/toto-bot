43288790825be09263dcdfb36a36d44a3e1d931f
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2024-02-29 17:58:04 +0300

    fix: move login logic to useLogin hook
    Move the login logic from the login page to useLogin hook to abstract
    the logic from the view

---
+++ src/app/hooks/useLogin.ts
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { supabase } from "../utils/supabase";
+import {  toast, Slide } from "react-toastify";
+export const FormSchema = z.object({
+  email: z
+    .string()
+    .email()
+    .refine((email) => email.trim().length > 0, {
+      message: "Email cannot be empty",
+    })
+    .refine((email) => email.trim().length < 50, {
+      message: "Email cannot be more than 50 characters",
+    })
+    .refine(
+      (email) => /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email),
+      {
+        message: "Please enter a valid email",
+      }
+    )
+    .refine(
+      (email) =>
+        /^\s*([a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})\s*$/.test(email),
+      {
+        message: "Email cannot contain leading or trailing spaces",
+      }
+    ),
+  password: z
+    .string()
+    .refine((password) => password.trim().length > 0, {
+      message: "Password cannot be empty",
+    })
+    .refine((password) => password.trim().length > 5, {
+      message: "Password must be at least 6 characters long",
+    })
+
+    .refine((password) => password.trim().length < 50, {
+      message: "Password cannot be more than 50 characters",
+    }),
+});
+export type FormData = z.infer<typeof FormSchema>;
+function useLogin() {
+
+        const {
+          register,
+          handleSubmit,
+          formState: { errors },
+        } = useForm<FormData>({
+          resolver: zodResolver(FormSchema),
+        });
+        const router = useRouter();
+      
+        const [isLoading, setIsLoading] = useState(false);
+      
+        const handleLogin = async (data: FormData) => {
+          try {
+            setIsLoading(true);
+            await supabase.auth.signInWithPassword({
+              email: data.email,
+              password: data.password,
+            });
+            toast.success("SignedIn!", {
+              position: "top-center",
+              autoClose: 10000,
+              hideProgressBar: true,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "colored",
+              transition: Slide,
+            });
+            router.refresh();
+          } catch (error) {
+            console.log(error);
+            toast.error("An Error Occurred!", {
+              position: "top-center",
+              autoClose: 2000,
+              hideProgressBar: true,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "colored",
+              transition: Slide,
+            });
+          } finally {
+            setIsLoading(false);
+          }
+        };
+        return {register,handleSubmit,errors,isLoading,handleLogin}
+      }
+        
+
+
+  
+
+
+export default useLogin;
