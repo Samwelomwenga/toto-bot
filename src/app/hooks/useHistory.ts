b319bcebd4e45dbabb3380135c07745a8e27892e
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2024-03-04 23:28:37 +0300

    fix: change useHistory to fetch only data where the user is the owner
    Change useHistory to fetch only data where the user is the owner and order
    by created_at in descending order  to show the most recent data first

--- src/app/hooks/useHistory.ts
+++ src/app/hooks/useHistory.ts
@@ -2,43 +2,49 @@ import { use, useEffect, useState } from "react";
 import { supabase } from "../utils/supabase";
 
 type HistoryState = {
-    loading: boolean;
-    error: { message: string,isError:boolean } | null;
-    history: Conversation[];
+  loading: boolean;
+  error: { message: string; isError: boolean } | null;
+  history: Conversation[];
 };
 
 function useHistory() {
-    const [historyState, setHistorySate] = useState<HistoryState>({
-        loading: false,
-        error: null,
-        history: [],
-    });
-    useEffect(() => {
+  const [historyState, setHistorySate] = useState<HistoryState>({
+    loading: false,
+    error: null,
+    history: [],
+  });
+  useEffect(() => {
     const getChatHistory = async () => {
-        try {
-            setHistorySate(prevState=>({ ...prevState, loading: true }));
-            const { data, error } = await supabase.from("conversations").select("*");
-            if (error) {
-              throw new Error(error.message);
-            }
-            setHistorySate(prevState=>({ ...prevState,history: data }));
+      try {
+        setHistorySate((prevState) => ({ ...prevState, loading: true }));
+        const userUId = (await supabase.auth.getSession()).data.session?.user
+          .id;
+          if (!userUId) {
+            throw new Error("User UID is not available");
+          }
+
+        const { data, error } = await supabase
+          .from("conversations")
+          .select("*")
+          .eq("User UID", userUId).order("created_at", { ascending: false});
+        if (error) {
+          throw new Error(error.message);
         }
-        catch (e) {
-            const error = e as Error;
-            setHistorySate(prevState=>({
-                ...prevState,
-                error: { message: error.message, isError: true },
-            }));
-        }finally{
-            setHistorySate(prevState=>({...prevState,loading:false}))
-        }
+        setHistorySate((prevState) => ({ ...prevState, history: data }));
+      } catch (e) {
+        const error = e as Error;
+        setHistorySate((prevState) => ({
+          ...prevState,
+          error: { message: error.message, isError: true },
+        }));
+      } finally {
+        setHistorySate((prevState) => ({ ...prevState, loading: false }));
+      }
     };
     getChatHistory();
-    }, []);
-     
-    return historyState;
+  }, []);
 
-
+  return historyState;
 }
 
-export default useHistory
+export default useHistory;
