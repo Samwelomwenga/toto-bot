c6482b24a08d10cb3915bf6afb58b06335ad5f51
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2024-03-05 23:58:40 +0300

    fix: delete history Fix delete history feature by debugging the useDeleteHistory hook and the History component

--- src/app/hooks/useDeleteHistory.ts
+++ src/app/hooks/useDeleteHistory.ts
@@ -1,4 +1,4 @@
-import { toast } from "react-toastify";
+import { Slide, toast } from "react-toastify";
 import { supabase } from "../utils/supabase";
 
 function useDeleteHistory() {
@@ -12,10 +12,31 @@ function useDeleteHistory() {
         console.log(error);
         throw new Error(error.message);
       }
+      toast.success("History Deleted Successfully", {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Slide,
+      });
     } catch (e) {
       const error = e as Error;
       console.log(error);
-      toast.
+      toast.error(`${error.message}`, {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Slide,
+      });
     }
   };
   return { handleDeleteHistory };
--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,6 +1,8 @@
+import { MdOutlineDeleteForever } from "react-icons/md";
 import { Message } from "@/app/hooks/useChatHandlers";
 import { ChatHistoryProps } from "./ChatHistory";
 import useHistoryClick from "@/app/hooks/useHistoryClick";
+import useDeleteHistory from "@/app/hooks/useDeleteHistory";
 
 type HistoryProps = {
   conversation_id: string;
@@ -14,14 +16,15 @@ function History({
 }: HistoryProps) {
   const { handleHistoryClick } = useHistoryClick({ chatState, dispatch });
   const parsedMessages: Message[] = JSON.parse(messages);
+  const handleDeleteHistory = useDeleteHistory();
   return (
-    <div
-      className="p-2 border-b-2 border-gray-600 hover:cursor-pointer"
-      onClick={() => handleHistoryClick(conversation_id)}
-    >
-      <p className="text-xs t whitespace-nowrap overflow-x-hidden overflow-ellipsis">
-        {parsedMessages[0].content}
-      </p>
+    <div className="p-2 border-b-2 border-gray-600 hover:cursor-pointer flex justify-between">
+      <div onClick={() => handleHistoryClick(conversation_id)}>
+        <p className="text-xs t whitespace-nowrap overflow-x-hidden overflow-ellipsis">
+          {parsedMessages[0].content}
+        </p>
+      </div>
+      <MdOutlineDeleteForever onClick={()=>handleDeleteHistory(conversation_id)} />
     </div>
   );
 }
