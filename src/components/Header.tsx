4da28419224a2903ac025368c498a7cb46d4add5
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2024-03-10 23:46:53 +0300

    feat:add logo fonts
    Add logo fonts to the project and use them in the header component  to
    display the logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,16 @@
 "use client";
 import { SlMenu } from "react-icons/sl";
 import { VscChromeClose } from "react-icons/vsc";
-// import Menu from "./Menu";
+import { Rubik_Doodle_Shadow} from "next/font/google";
+
+const rubik = Rubik_Doodle_Shadow({weight:"400", subsets: ["latin"] });
+
 function Header({ isOpen, toggle}:{isOpen: boolean, toggle: () => void}) {
  
   return (
     <header className="text-black bg-teal-600 h-12 w-full px-3 py-5 shadow-lg sticky md:hidden">
       <div className="flex justify-between mb-8">
-        <p className="text-2xl font-semibold text-orange-400">TOTO BOT</p>
+        <p className={`text-2xl font-semibold text-orange-400 ${rubik.className}`}>TOTO BOT</p>
         {!isOpen ? (
           <SlMenu
           className={` w-7 h-7 text-orange-400 transition-transform duration-700 ease-in-out ${isOpen ? 'opacity-0 scale-0' : 'opacity-100 scale-100'}`}
