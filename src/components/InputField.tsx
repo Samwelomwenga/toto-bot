754cc26b73eea2848cf23eecb983ff56324d9b75
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2024-03-15 23:49:47 +0300

    feat: add show password icon to password input field
    Add a show password icon to the password input field to allow users to
    toggle the visibility of the password

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,32 +1,40 @@
+import { useState } from "react";
 import { UseFormRegister } from "react-hook-form";
-import { IoEye,IoEyeOff } from "react-icons/io5";
+import { IoEye, IoEyeOff } from "react-icons/io5";
 
 type InputFieldProps = {
-    register: UseFormRegister<{
-        name: string;
-        email: string;
-        password: string;
-    }>,
-    placeholder: string,
-    type: string,
-    name: "name" | "email" | "password",
-}
-function InputField({ register, placeholder, type,name }: InputFieldProps) {
-    const [showPassword,setShowPassword]=useState(false)
+  register: UseFormRegister<{
+    name: string;
+    email: string;
+    password: string;
+  }>;
+  placeholder: string;
+  type: string;
+  name: "name" | "email" | "password";
+};
+function InputField({ register, placeholder, type, name }: InputFieldProps) {
+  const [showPassword, setShowPassword] = useState(false);
 
-    const handleShowPassword=()=>{
-        setShowPassword(!showPassword)
-    }
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
   return (
-    <>
+    <div className="relative">
       <input
-        {...register( name)}
+        {...register(name)}
         className="block  w-full my-2 py-3 px-4 rounded"
         placeholder={placeholder}
-        type={type}
+        type={type === "password" ? (showPassword ? "text" : "password") : type}
       />
-      <IoEye/>
-    </>
+      {type == "password" ? (
+        <div
+          className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+          onClick={handleShowPassword}
+        >
+          {showPassword ? <IoEyeOff size={24} /> : <IoEye size={24} />}
+        </div>
+      ) : null}
+    </div>
   );
 }
 
