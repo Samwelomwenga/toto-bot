9cf082de422f14f9f999876510028bf339eb8aca
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2024-03-21 23:22:39 +0300

    fix: change InputFieldProps to generic type
    Change InputFieldProps to generic type to allow for more flexibility in
    the type of the input field

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,23 +1,20 @@
 import { useState } from "react";
-import { UseFormRegister } from "react-hook-form";
+import { FieldPath, FieldValues, UseFormRegister } from "react-hook-form";
 import { IoEye, IoEyeOff } from "react-icons/io5";
 
-type SignUpFieldProps = UseFormRegister<{
-  name: string;
-  email: string;
-  password: string;
-}>;
-type loginFieldProps = UseFormRegister<{
-  email: string;
-  password: string;
-}>;
-type InputFieldProps = {
-  register: SignUpFieldProps;
+//
+type InputFieldProps<TFieldValues extends FieldValues> = {
+  register: UseFormRegister<TFieldValues>;
   placeholder: string;
   type: string;
-  name: "name" | "email" | "password";
+  name: FieldPath<TFieldValues>;
 };
-function InputField({ register, placeholder, type, name }: InputFieldProps) {
+function InputField<TFieldsValues extends FieldValues>({
+  register,
+  placeholder,
+  type,
+  name,
+}: InputFieldProps<TFieldsValues>) {
   const [showPassword, setShowPassword] = useState(false);
 
   const handleShowPassword = () => {
@@ -26,9 +23,6 @@ function InputField({ register, placeholder, type, name }: InputFieldProps) {
   return (
     <div className="relative">
       <input
-      // Error
-      // This expression is not callable.
-      // Each member of the union type 'SignUpFieldProps | loginFieldProps' has signatures, but none of those signatures are compatible with each other.ts(2349)
         {...register(name)}
         className="block  w-full my-2 py-3 px-4 rounded"
         placeholder={placeholder}
